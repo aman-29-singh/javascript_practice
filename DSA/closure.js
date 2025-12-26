7ccddaeab5018d71116cc8fc8a40eef710018e9e
aman-29-singh <singhaman41004@gmail.com>
2025-12-26 20:40:48 +0530

    adding some comments for learning

--- DSA/closure.js
+++ DSA/closure.js
@@ -15,3 +15,10 @@ console.log(counter)//so abb counter variable k andar increment ka refrence hai 
 console.log(counter());//it will give 1
 console.log(counter());//it will give 2
 console.log(counter());//it will give 3
+console.log(counter());//Note ye counter() ka matlab hai increment() ko call karna
+/**so console.log(counter) means hai ki ismein counter mein bas increment function ka reference hai
+ * so yeh reference se function increment call nhi hota hai because function call karne k liye humein ()
+ * () parenthesis ka use karna padega 
+ * aur console.log(counter()) mein idhar counter() ka matlab hai ki ismein increment() aise call kiye hai
+ * 
+ */
